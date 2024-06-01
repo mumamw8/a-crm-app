4040ff18a5117e2a9af45883721eec568624aa22
mumamw8 <91581448+mumamw8@users.noreply.github.com>
2024-05-31 19:33:32 -0500

    feat(api): add pagination support to GET endpoint

---
+++ src/app/api/person/route.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/dot-notation */
+import { db } from "@/server/db";
+import { company } from "@/server/db/schema";
+import { count } from "drizzle-orm";
+
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  // const body = await request.json();
+  const page = parseInt(searchParams.get('pageNumber') ?? "1")
+  const pageSize = parseInt(searchParams.get('pageSize') ?? "10");
+
+  const skip = (page - 1) * pageSize;
+
+  // const totalCount = await db
+  //   .select({ count: count() })
+  //   .from(company)
+  //   .orderBy(company.name);
+  const result = await db
+    .select()
+    .from(company)
+    .orderBy(company.name)
+    .limit(pageSize)
+    .offset(skip);
+
+  return Response.json({ data: result });
+}
